6b0f2e746c96d28833f5dbb6d81e527f3b287952
Marius Andra <marius.andra@gmail.com>
2023-08-22 23:06:57 +0200

    remove favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "FrameOS",
   tagline: "Control software for Raspberry Pi e-ink displays",
-  favicon: "img/favicon.ico",
+  // favicon: "img/favicon.ico",
 
   // Set the production url of your site here
   url: "https://frameos.net",
