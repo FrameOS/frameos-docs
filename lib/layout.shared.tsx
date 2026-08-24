36594e411c31d076ff23848882e1c5f687fbed49
Marius Andra <marius.andra@gmail.com>
2026-08-25 00:48:07 +0200

    scenes

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -18,6 +18,7 @@ export function baseOptions(): BaseLayoutProps {
       { text: 'Devices', url: '/devices', active: 'nested-url' },
       { text: 'Cases', url: '/cases', active: 'nested-url' },
       { text: 'Blog', url: '/blog', active: 'nested-url' },
+      { text: 'Scenes', url: links.sceneStore, external: true },
       { text: 'Cloud', url: links.cloud, external: true },
     ],
     githubUrl: links.github,
